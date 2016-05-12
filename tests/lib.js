1d5b794534e2bf47b241a24a9470f8151b7dbdb4
Evo Stamatov <aviolito@gmail.com>
2016-05-12 12:53:58 +1000

    add first tests

---
+++ tests/lib.js
@@ -0,0 +1,23 @@
+var test = require('tape');
+var fixme = require('..');
+
+function dummy () {}
+var baseOptions = {
+  path: 'test',
+  loggerFunc: dummy
+}
+
+test('fixme is a function', function (t) {
+  t.equal(typeof fixme, 'function');
+  t.end();
+});
+
+test('fixme accepts options', function (t) {
+  t.doesNotThrow(function() { fixme(baseOptions); });
+  t.end();
+});
+
+test('fixme returns a thenable', function (t) {
+  t.equal(typeof fixme(baseOptions).then, 'function');
+  t.end();
+});
